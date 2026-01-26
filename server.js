804989747431aca76c451d6335839986be6d2386
Oscar Castellanos <ocastell24@gmail.com>
2026-01-25 19:58:55 -0500

    Add x-user-id to CORS allowed headers

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
